cfc00601fa2ee1ef852a78bb36d08dbd26ef0516
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-11-06 20:48:07 +0100

    methods on paths works

--- tests/e2e/tests/expose_methods_on_paths/expose_methods_on_paths_test.go
+++ tests/e2e/tests/expose_methods_on_paths/expose_methods_on_paths_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/domain"
 	infrahelpers "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/infrastructure"
 	modulehelpers "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/modules"
+	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/oauth2"
 	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/testsetup"
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/e2e-framework/klient/decoder"
@@ -71,53 +72,69 @@ func TestAPIRuleRequestHeadersAndCookies(t *testing.T) {
 		}
 	})
 
-	//t.Run("Expose GET, POST method for /anything and PUT for /anything/put secured by JWT", func(t *testing.T) {
-	//	testBackground, err := testsetup.SetupRandomNamespaceWithOauth2MockAndHttpbin(t, testsetup.WithPrefix("methods-on-paths"))
-	//	require.NoError(t, err, "Failed to setup test background with httpbin")
-	//
-	//	createdApirule, err := infrahelpers.CreateResourceWithTemplateValues(
-	//		t,
-	//		APIRuleJwt,
-	//		// got to fulfill these properly
-	//		map[string]any{
-	//			"Name":        testBackground.TestName,
-	//			"Host":        testBackground.TestName,
-	//			"ServiceName": testBackground.TargetServiceName,
-	//			"ServicePort": testBackground.TargetServicePort,
-	//			"Gateway":     "kyma-system/kyma-gateway",
-	//		},
-	//		decoder.MutateNamespace(testBackground.Namespace),
-	//	)
-	//	require.NoError(t, err, "Failed to create APIRule resource")
-	//	require.NotEmpty(t, createdApirule, "Created APIRule resource should not be empty")
-	//
-	//	apiruleasserts.WaitUntilReady(t, testBackground.TestName, testBackground.Namespace)
-	//	istioasserts.VirtualServiceOwnedByAPIRuleExists(t, testBackground.Namespace, testBackground.TestName, testBackground.Namespace)
-	//
-	//	requests := []struct {
-	//		path                   string
-	//		method                 string
-	//		expectedResponseStatus int
-	//	}{
-	//		{path: "/anything", method: http.MethodGet, expectedResponseStatus: http.StatusOK},
-	//		{path: "/anything", method: http.MethodPost, expectedResponseStatus: http.StatusOK},
-	//		{path: "/anything", method: http.MethodPut, expectedResponseStatus: http.StatusNotFound},
-	//		{path: "/anything/put", method: http.MethodPut, expectedResponseStatus: http.StatusOK},
-	//		{path: "/anything/put", method: http.MethodPost, expectedResponseStatus: http.StatusNotFound},
-	//	}
-	//
-	//	for _, r := range requests {
-	//		url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
-	//		req, err := http.NewRequest(r.method, url, nil)
-	//		if err != nil {
-	//			t.Fatalf("err %s", err.Error())
-	//		}
-	//		c := h.NewHTTPClient(t)
-	//		resp, err := c.Do(req)
-	//		if err != nil {
-	//			t.Fatalf("err %s", err.Error())
-	//		}
-	//		require.Equal(t, resp.StatusCode, r.expectedResponseStatus)
-	//	}
-	//})
+	t.Run("Expose GET, POST method for /anything and PUT for /anything/put secured by JWT", func(t *testing.T) {
+		t.Parallel()
+		testBackground, err := testsetup.SetupRandomNamespaceWithOauth2MockAndHttpbin(t, testsetup.WithPrefix("methods-on-paths"))
+		require.NoError(t, err, "Failed to setup test background with httpbin")
+
+		createdApirule, err := infrahelpers.CreateResourceWithTemplateValues(
+			t,
+			APIRuleJwt,
+			// got to fulfill these properly
+			map[string]any{
+				"Name":        testBackground.TestName,
+				"Host":        testBackground.TestName,
+				"ServiceName": testBackground.TargetServiceName,
+				"ServicePort": testBackground.TargetServicePort,
+				"Gateway":     "kyma-system/kyma-gateway",
+				"Issuer":      testBackground.Provider.GetIssuerURL(),
+				"JwksUri":     testBackground.Provider.GetJwksURI(),
+			},
+			decoder.MutateNamespace(testBackground.Namespace),
+		)
+		require.NoError(t, err, "Failed to create APIRule resource")
+		require.NotEmpty(t, createdApirule, "Created APIRule resource should not be empty")
+
+		apiruleasserts.WaitUntilReady(t, testBackground.TestName, testBackground.Namespace)
+		istioasserts.VirtualServiceOwnedByAPIRuleExists(t, testBackground.Namespace, testBackground.TestName, testBackground.Namespace)
+
+		exposedPathsRequests := []struct {
+			path                   string
+			method                 string
+			expectedResponseStatus int
+		}{
+			{path: "/anything", method: http.MethodGet, expectedResponseStatus: http.StatusOK},
+			{path: "/anything", method: http.MethodPost, expectedResponseStatus: http.StatusOK},
+			{path: "/anything/put", method: http.MethodPut, expectedResponseStatus: http.StatusOK},
+		}
+
+		for _, r := range exposedPathsRequests {
+			url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
+			oauth2.AssertEndpointWithProvider(
+				t,
+				testBackground.Provider,
+				url,
+				r.method,
+			)
+		}
+
+		nonExposedPathsRequests := []struct {
+			path                   string
+			method                 string
+			expectedResponseStatus int
+		}{
+			{path: "/anything", method: http.MethodPut, expectedResponseStatus: http.StatusNotFound},
+			{path: "/anything/put", method: http.MethodPost, expectedResponseStatus: http.StatusNotFound},
+		}
+
+		for _, r := range nonExposedPathsRequests {
+			url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
+			oauth2.AssertNonExposedEndpointWithProvider(
+				t,
+				testBackground.Provider,
+				url,
+				r.method,
+			)
+		}
+	})
 }
