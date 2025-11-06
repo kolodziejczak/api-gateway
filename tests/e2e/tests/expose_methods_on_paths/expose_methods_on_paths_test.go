a0c81a1e667630507969936c51f03dd5e28a3589
Marek Kolodziejczak <marek.kolodziejczak@sap.com>
2025-11-06 20:01:15 +0100

    expose methods first part works, now gonna to the secured one

--- tests/e2e/tests/expose_methods_on_paths/expose_methods_on_paths_test.go
+++ tests/e2e/tests/expose_methods_on_paths/expose_methods_on_paths_test.go
@@ -7,9 +7,9 @@ import (
 	"testing"
 
 	apiruleasserts "github.com/kyma-project/api-gateway/tests/e2e/pkg/asserts/apirule"
+	"github.com/kyma-project/api-gateway/tests/e2e/pkg/asserts/endpoint"
 	istioasserts "github.com/kyma-project/api-gateway/tests/e2e/pkg/asserts/istio"
 	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/domain"
-	h "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/http"
 	infrahelpers "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/infrastructure"
 	modulehelpers "github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/modules"
 	"github.com/kyma-project/api-gateway/tests/e2e/pkg/helpers/testsetup"
@@ -36,7 +36,6 @@ func TestAPIRuleRequestHeadersAndCookies(t *testing.T) {
 		createdApirule, err := infrahelpers.CreateResourceWithTemplateValues(
 			t,
 			APIRuleNoAuth,
-			// got to fulfill these properly
 			map[string]any{
 				"Name":        testBackground.TestName,
 				"Host":        testBackground.TestName,
@@ -66,66 +65,58 @@ func TestAPIRuleRequestHeadersAndCookies(t *testing.T) {
 
 		for _, r := range requests {
 			url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
-			req, err := http.NewRequest(r.method, url, nil)
-			if err != nil {
-				t.Fatalf("err %s", err.Error())
-			}
-			c := h.NewHTTPClient(t)
-			resp, err := c.Do(req)
-			if err != nil {
-				t.Fatalf("err %s", err.Error())
-			}
-			require.Equal(t, resp.StatusCode, r.expectedResponseStatus)
+			err = endpoint.AssertEndpoint(t, r.method, url, r.expectedResponseStatus)
+			require.NoError(t, err, "Failed to make http request")
 		}
 	})
 
-	t.Run("Expose GET, POST method for /anything and PUT for /anything/put secured by JWT", func(t *testing.T) {
-		testBackground, err := testsetup.SetupRandomNamespaceWithOauth2MockAndHttpbin(t, testsetup.WithPrefix("methods-on-paths"))
-		require.NoError(t, err, "Failed to setup test background with httpbin")
-
-		createdApirule, err := infrahelpers.CreateResourceWithTemplateValues(
-			t,
-			APIRuleJwt,
-			// got to fulfill these properly
-			map[string]any{
-				"Name":        testBackground.TestName,
-				"Host":        testBackground.TestName,
-				"ServiceName": testBackground.TargetServiceName,
-				"ServicePort": testBackground.TargetServicePort,
-				"Gateway":     "kyma-system/kyma-gateway",
-			},
-			decoder.MutateNamespace(testBackground.Namespace),
-		)
-		require.NoError(t, err, "Failed to create APIRule resource")
-		require.NotEmpty(t, createdApirule, "Created APIRule resource should not be empty")
-
-		apiruleasserts.WaitUntilReady(t, testBackground.TestName, testBackground.Namespace)
-		istioasserts.VirtualServiceOwnedByAPIRuleExists(t, testBackground.Namespace, testBackground.TestName, testBackground.Namespace)
-
-		requests := []struct {
-			path                   string
-			method                 string
-			expectedResponseStatus int
-		}{
-			{path: "/anything", method: http.MethodGet, expectedResponseStatus: http.StatusOK},
-			{path: "/anything", method: http.MethodPost, expectedResponseStatus: http.StatusOK},
-			{path: "/anything", method: http.MethodPut, expectedResponseStatus: http.StatusNotFound},
-			{path: "/anything/put", method: http.MethodPut, expectedResponseStatus: http.StatusOK},
-			{path: "/anything/put", method: http.MethodPost, expectedResponseStatus: http.StatusNotFound},
-		}
-
-		for _, r := range requests {
-			url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
-			req, err := http.NewRequest(r.method, url, nil)
-			if err != nil {
-				t.Fatalf("err %s", err.Error())
-			}
-			c := h.NewHTTPClient(t)
-			resp, err := c.Do(req)
-			if err != nil {
-				t.Fatalf("err %s", err.Error())
-			}
-			require.Equal(t, resp.StatusCode, r.expectedResponseStatus)
-		}
-	})
+	//t.Run("Expose GET, POST method for /anything and PUT for /anything/put secured by JWT", func(t *testing.T) {
+	//	testBackground, err := testsetup.SetupRandomNamespaceWithOauth2MockAndHttpbin(t, testsetup.WithPrefix("methods-on-paths"))
+	//	require.NoError(t, err, "Failed to setup test background with httpbin")
+	//
+	//	createdApirule, err := infrahelpers.CreateResourceWithTemplateValues(
+	//		t,
+	//		APIRuleJwt,
+	//		// got to fulfill these properly
+	//		map[string]any{
+	//			"Name":        testBackground.TestName,
+	//			"Host":        testBackground.TestName,
+	//			"ServiceName": testBackground.TargetServiceName,
+	//			"ServicePort": testBackground.TargetServicePort,
+	//			"Gateway":     "kyma-system/kyma-gateway",
+	//		},
+	//		decoder.MutateNamespace(testBackground.Namespace),
+	//	)
+	//	require.NoError(t, err, "Failed to create APIRule resource")
+	//	require.NotEmpty(t, createdApirule, "Created APIRule resource should not be empty")
+	//
+	//	apiruleasserts.WaitUntilReady(t, testBackground.TestName, testBackground.Namespace)
+	//	istioasserts.VirtualServiceOwnedByAPIRuleExists(t, testBackground.Namespace, testBackground.TestName, testBackground.Namespace)
+	//
+	//	requests := []struct {
+	//		path                   string
+	//		method                 string
+	//		expectedResponseStatus int
+	//	}{
+	//		{path: "/anything", method: http.MethodGet, expectedResponseStatus: http.StatusOK},
+	//		{path: "/anything", method: http.MethodPost, expectedResponseStatus: http.StatusOK},
+	//		{path: "/anything", method: http.MethodPut, expectedResponseStatus: http.StatusNotFound},
+	//		{path: "/anything/put", method: http.MethodPut, expectedResponseStatus: http.StatusOK},
+	//		{path: "/anything/put", method: http.MethodPost, expectedResponseStatus: http.StatusNotFound},
+	//	}
+	//
+	//	for _, r := range requests {
+	//		url := fmt.Sprintf("https://%s.%s%s", testBackground.TestName, kymaGatewayDomain, r.path)
+	//		req, err := http.NewRequest(r.method, url, nil)
+	//		if err != nil {
+	//			t.Fatalf("err %s", err.Error())
+	//		}
+	//		c := h.NewHTTPClient(t)
+	//		resp, err := c.Do(req)
+	//		if err != nil {
+	//			t.Fatalf("err %s", err.Error())
+	//		}
+	//		require.Equal(t, resp.StatusCode, r.expectedResponseStatus)
+	//	}
+	//})
 }
